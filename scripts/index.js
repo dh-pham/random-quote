255bb02323aced7b210ef11470c0272b721c06cb
hienhack96 <hpd.hienpham@gmail.com>
2018-03-26 22:46:05 +0700

    done add quote

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,29 @@
  * - All our useful JS goes here, awesome!
  */
 
+function randomQuote() {
+  $.ajax( {
+    url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
+    jsonp: 'jsonp',
+    dataType: 'jsonp',
+    data: {
+      method: 'getQuote',
+      lang: 'en',
+      format: 'jsonp'
+    },
+    // jsonpCallback: function(data) {
+    //   document.querySelector("#text").html(data);
+    // },
+    success: function(data) {
+      console.log(data);
+      $("#quoteText").html("<p>" + data.quoteText + "</p>");
+      $("#quoteAuthors").html("<p>" + " -" + data.quoteAuthor + "</p>");
+    }
+  });
+}
+
 $(document).ready(function() {
-    document.querySelector("#new-quote").addEventListener("click", function() {
-      alert("hello, hien pham !");
-        $.ajax( {
-          url: "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
-          dataType: "json",
-          success: function(data) {
-            console.log(data);
-          }
-        });
-    });
+  $("#new-quote").on("click", randomQuote);
 });
+
+randomQuote();
